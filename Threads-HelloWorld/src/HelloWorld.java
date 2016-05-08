3ebd7bfc69f96ec1718b9c3c9533779b53c0338a
creationk <creationk@srujana-Inspiron-3521>
2016-05-08 12:41:14 +0530

    hello world!

---
+++ Threads-HelloWorld/src/HelloWorld.java
@@ -0,0 +1,30 @@
+
+public class HelloWorld extends Thread {
+	public static void main (String [] args){
+		world();
+		hello();
+	}
+
+	private static void hello() {
+		HelloWorld hello=new HelloWorld() {
+			public void run () {
+				System.out.println("hello");
+			}
+		};
+		hello.start();
+	}
+
+	private static void world() {
+		HelloWorld world=new HelloWorld() {
+			public void run () {
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("world");
+			}
+		};
+		world.start();
+	}
+}
